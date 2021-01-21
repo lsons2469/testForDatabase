19ef44728b00769511779f3ca8a9c97a1c2b6b95
lsons2469 <lsonshrestha@gmail.com>
2021-01-21 12:45:40 -0600

    Added test Project

---
+++ CapitalsOfTheWorld/src/main/java/com/abc/app/CapitalsOfTheWorld.java
@@ -0,0 +1,13 @@
+package com.abc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalsOfTheWorld {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalsOfTheWorld.class, args);
+	}
+
+}
---
+++ CapitalsOfTheWorld/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplciation.java
@@ -0,0 +1,12 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataMysqlApplciation {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataMysqlApplciation.class, args);
+    }
+}
